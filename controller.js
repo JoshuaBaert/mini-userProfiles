480389fb52cefe9ae77fea200b6e5b77ab4c5a7b
Joshua Baert <jbghoring@gmail.com>
2016-10-25 12:27:39 -0600

    Finished

--- controller.js
+++ controller.js
@@ -1,5 +1,11 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
-    $scope.thisAppIsBroken = "This angular app is working";
-   
-})
+angular.module('userProfiles').controller('MainController', function ($scope, mainService) {
+	
+	$scope.thisAppIsBroken = "This angular app is working";
+	
+	$scope.getUsers = function () {
+		return mainService.getUsers();
+	};
+	$scope.users = $scope.getUsers();
+	$scope.toggleFavorite = mainService.toggleFavorite;
+	
+});
